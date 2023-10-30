32862b37849d9f0580d44ee2efdea1621eddaa43
Picka <marmong@naver.com>
2023-10-30 13:34:34 +0900

    그리디 알고리즘

---
+++ Class3/1393.py
@@ -0,0 +1,23 @@
+t = int(input())
+import sys
+meeting_list = []
+for i in range(t):
+    start ,end = map(int, sys.stdin.readline().strip().split())
+    meeting_list.append([start,end])
+    
+
+meeting_list.sort(key=lambda x : (x[1],x[0]))
+count = 1
+i = 1
+
+j = 0
+while i < t and j < t:
+    end_time = meeting_list[j][1]
+    start_time = meeting_list[i][0]
+    if end_time <= start_time:
+        count += 1
+        j = i
+    i += 1
+        
+        
+print(count)
---
+++ Class3/2630.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(input())
+
+arr = [list(map(int, input().split()))for _ in range(n)]
+
+def search_blue(n,arr):
+    # 1사분면
+    
+    # 2사분면
+    # 3사분면
+    # 4사분면
