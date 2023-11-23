b741135cbb7245047571852dc2a002b197a697a8
Picka <marmong@naver.com>
2023-11-23 17:26:14 +0900

    update

--- Class4/1149.py
+++ Class4/1149.py
@@ -1,9 +1,15 @@
 import sys
 
 n = int(input())
-arr = []
 lines = sys.stdin.readlines()
 dp = [[0] * 3 for _ in range(n)]
 
-for line in lines:
-    
+for i in range(n):
+    num = list(map(int, lines[i].split()))
+    for j in range(3):
+        if i == 0:
+            dp[i][j] = num[j]
+        else:
+            dp[i][j] = min(dp[i - 1][:j] + dp[i - 1][j + 1:]) + num[j]
+
+print(min(dp[n - 1]))
